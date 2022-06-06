a2f8aa240ec66a27e2977a0c7f6264a4f44e815b
Sabiduria10 <oyinseyeshola@gmail.com>
2022-06-06 12:34:46 +0000

    List Tuples - Data Structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # 7-add_tuple.py
-# Brennan D Baraban
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """Add two tuples."""
     if len(tuple_a) < 2:
-        if len(tuple_b) == 0:
+        if len(tuple_a) == 0:
             tuple_a = 0, 0
         else:
             tuple_a = tuple_a[0], 0
