ac6750c8327283650dffab0fd778e7fc0363b0e7
Sabiduria10 <oyinseyeshola@gmail.com>
2022-06-25 11:20:26 +0000

    More on Data Structures in C

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+# 5-number_keys.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
