046c38759a58316219d68c5ba6762d584a9a9a7e
Sabiduria10 <oyinseyeshola@gmail.com>
2022-06-25 11:26:49 +0000

    More on Data Structures in C

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,7 @@
 # 6-print_sorted_dictionary.py
 # Brennan D Baraban <375@holbertonschool.com>
 
-def print_stored_dictionary(a_dictionary):
+
+def print_sorted_dictionary(a_dictionary):
     """Print a dictionary by ordered keys."""
     [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
