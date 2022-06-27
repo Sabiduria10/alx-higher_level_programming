40d85a192cad1778e56ba552eb1eb30b47fffd9a
Sabiduria10 <oyinseyeshola@gmail.com>
2022-06-27 11:32:02 +0000

    More on Data Structures in C

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -16,7 +16,7 @@ void print_python_list(PyObject *p)
 {
 	int size, alloc, i;
 	const char *type;
-	PyObject *list = (PyListObject *)p;
+	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
 
 	size = var->ob_size;
@@ -44,26 +44,26 @@ void print_python_bytes(PyObject *p)
 	unsigned char i, size;
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
-	printf("'[.] bytes object info\n");
+	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
-		printf("   [ERROR] Invalide Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	printf("    size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("    trying string: %s\n", bytes->ob_sval);
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
 
 	if (((PyVarObject *)p)->ob_size > 10)
 		size = 10;
 	else
 		size = ((PyVarObject *)p)->ob_size + 1;
 
-	printf("    first %d bytes: ", size);
+	printf("  first %d bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
-		printf("%02hhx", bytes-ob_sval[i]);
-		if (i == size - 1))
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i == (size - 1))
 			printf("\n");
 		else
 			printf(" ");
